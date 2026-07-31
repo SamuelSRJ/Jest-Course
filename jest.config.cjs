26e98792dba64c4de5a7a0d5d2b491737e45b2d3
SamuelSRJ <samuelsouzarosajr@gmail.com>
2026-07-31 16:46:08 -0300

    add: DataBase tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 
-const baseDir = '<rootDir>/src/app/doubles';
-const baseTestDir = '<rootDir>/src/test/doubles';
+const baseDir = '<rootDir>/src/app/server_app';
+const baseTestDir = '<rootDir>/src/test/server_app';
 
 module.exports = {
   testEnvironment: 'node',
