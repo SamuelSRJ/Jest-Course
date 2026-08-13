558fc6d756d358eb1b155e71ccfcb33fab826bc8
SamuelSRJ <samuelsouzarosajr@gmail.com>
2026-08-13 17:51:15 -0300

    add: integration tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 
 const baseDir = '<rootDir>/src/app/server_app/';
-const baseTestDir = '<rootDir>/src/test/server_app2/';
+const baseTestDir = '<rootDir>/src/test/server_app3/';
 
 module.exports = {
   testEnvironment: 'node',
